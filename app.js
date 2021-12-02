d24a7ae42e655593105807664ae5a24a427e5254
psambalkar <parth.ambalkar16@gmail.com>
2021-12-02 17:20:37 -0500

    added tourschemamodel tourroutes and login form views and app.js and server.js

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
+const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
 const hpp = require("hpp")
+const cors=require("cors");
+const xss = require("xss-clean")
+const cookieParser = require('cookie-parser')
 const tourRoutes = require("./routes/tourRoutes")
 const userRoutes = require("./routes/userRoutes")
 const reviewRoutes = require("./routes/reviewRoutes")
+const viewRoutes = require("./routes/viewRoutes")
 const app = express();
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.enable('trust proxy');
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname,'public')));
 
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+}
+app.use(cookieParser())
+
+app.use(cors(corsOptions)) 
+app.options('*', cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use(hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  }))
+  app.use(xss());
+  app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    console.log(req.cookies);
+    next();
+  });
+app.use('/',viewRoutes)
+app.use('/travel-bliss/tours',tourRoutes);
+app.use('/travel-bliss/users',userRoutes);
+app.use('/travel-bliss/reviews',reviewRoutes);
+
+app.all("*",(req,res,next)=>{
+    res.status(404).json({
+        message:`No such Route ${req.originalUrl} Found on server`
+    })
+})
 module.exports = app
---
+++ models/tourSchemaModel.js
@@ -0,0 +1,115 @@
+const mongoose = require("mongoose")
+const slugify = require('slugify');
+const validator = require("validator")
+const tourSchemaModel = new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true,"A tour must have a name"],
+        unique:true,
+        trim:true,
+        minlength:[10,"The tour name must be atleast 10 character long "],
+        maxlength:[45,"The tour name must be less than 40 character long "],
+        // validate:[validator.isAlpha,"The Tour Name must Only have characters"]
+
+      },
+    price:{
+    type:Number,
+    required:[true,"A tour must have a price"],
+    validate:{       //this validator only runs when new docs are being created not works on update.
+      validator:function(val){
+        return val>0
+      },
+      message:"Price  should be greater than zero"
+    }
+    },
+    duration:{
+    type:Number,
+    required:[true,"A tour must have a duration as Number"],
+    validate:{       //this validator only runs when new docs are being created not works on update.
+      validator:function(val){
+        return val>0
+      },
+      message:"Duration should be greater than zero"
+    }
+    },
+    maxGroupSize:{
+    type:Number,
+    required:[true,"A tour must have a  GroupSize "]
+    },
+    difficulty:{
+    type:String,
+    required:[true,"A tour must have a difficulty field"],
+    enum:{
+      values:["difficult","easy","medium"],
+      message:"difficulty should be either difficult,easy or medium"
+    }
+    },
+    ratingsAverage:{
+    type:Number,
+    default:4,
+    max:[5,"rating must be below 5.0"],
+    min:[1,"rating must be above 1.0"],
+    set:value=> Math.round(value * 10) / 10
+    },
+    ratingsQuantity:{
+    type:Number,
+    default:0,
+    },
+    summary:{
+    type:String,
+    required:[true,"A tour must have a summary field"],
+    trim:true
+    },
+    description:{
+    type:String,
+    trim:true
+    },
+    imageCover:{
+    type:String,
+    required:[true,"A tour must have acover image field"]
+    },
+    startDates:[Date],
+    createdAt:{
+    type:Date,
+    default:Date.now(),
+    select:false
+    },
+    images:[String],
+    slug:String,
+    guides:[
+      {
+        type:mongoose.Schema.ObjectId,
+        ref:'User'
+      }
+    ]
+},{
+  toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+})
+
+tourSchemaModel.virtual('reviews',{
+  ref:"Review",
+  foreignField:"tour",
+  localField:"_id"
+})
+const presavefunction = function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+}
+tourSchemaModel.pre('save',function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+const populatingFeilds = function(next){
+  this.populate({
+    path: "guides",
+    select : "-passwordChangedAt -__v"
+     
+   }
+     );
+     next()
+}
+tourSchemaModel.pre(/^find/,populatingFeilds)
+const Tour = mongoose.model('Tour', tourSchemaModel);
+
+module.exports = Tour;
