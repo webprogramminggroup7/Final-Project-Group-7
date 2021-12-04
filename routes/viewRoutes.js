1242a592f3185265b9e232fcde9efb099acd1837
bhargav-baleja <bhargav.baleja.11@gmail.com>
2021-12-03 19:37:10 -0500

    Added a properly working signup form

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,8 +5,9 @@ const authenticationUser = require("../data/authenticationUser")
 const router = express.Router()
 
 router.use(authenticationUser.isLoggedIn)
+router.get('/signup',viewRoutes.SignUpForm)
 router.get("/",viewRoutes.landingPage)
 router.get("/tour/:slug",viewRoutes.tourViewPage)
 router.get('/login',viewRoutes.LoginForm)
-router.get('/signup',viewRoutes.SignUpForm)
+
 module.exports = router
