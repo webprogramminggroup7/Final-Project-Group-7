226d61f581d8da0977df67bfd56aeb2dcd8ec1d4
psambalkar <parth.ambalkar16@gmail.com>
2021-12-05 19:56:18 -0500

    commiting changes for update user and password with error handling

--- public/js/updateUserData.js
+++ public/js/updateUserData.js
@@ -1,30 +1,77 @@
-const updateSettings = async (data, type) => {
+const updateSettings = async (name, email) => {
     try {
-      const url =
-        type === 'password'
-          ? '/travel-bliss/users/updatePassword'
-          : '/travel-bliss/users/updateMyData';
+      // const url =
+      //   type === 'password'
+      //     ? '/travel-bliss/users/updatePassword'
+      //     : '/travel-bliss/users/updateMyData';
   
       const res = await axios({
         method: 'PATCH',
-        url,
-        data
+        url:'/travel-bliss/users/updateMyData',
+        data:{
+          name,email
+        }
       });
-  
-      if (res.data.status === 'success') {
-        alert(`success'${type.toUpperCase()} updated successfully!`);
+      // console.log(res)
+      if (res.data.status === 'successful in updating the name or email') {
+        alert(`successfully updated updated successfully!`);
       }
     } catch (err) {
-      alert(err.response.data.message);
+      console.log(err.response.data.message.message)
+      alert(err.response.data.message.message);
     }}
+const userDataForm = document.querySelector('.form-user-data')
 if (userDataForm){
   userDataForm.addEventListener('submit', e => {
  e.preventDefault();
 const name = document.getElementById('name').value;
 const email = document.getElementById('email').value;
-updateSettings({ name, email }, 'data');
+// console.log(name,email)
+updateSettings( name, email);
 
       });
 }
-}
- 
+const updatePassword = async (passwordCurrent, passwordConfirm,password) => {
+  try {
+    // const url =
+    //   type === 'password'
+    //     ? '/travel-bliss/users/updatePassword'
+    //     : '/travel-bliss/users/updateMyData';
+
+    const res = await axios({
+      method: 'PATCH',
+      url:'/travel-bliss/users/updatePassword',
+      data:{
+        passwordConfirm,passwordCurrent,password
+      }
+    });
+    // console.log(res)
+    if (res.data.status === 'Succesfully Logged In') {
+      alert(`successfully updated the Password!`);
+    }else{
+      alert(`${res.data.message}!`);
+
+    }
+    console.log(res.data.message)
+  } catch (err) {
+    if(err.response.data.message==="Your current password is wrong"){
+      alert(err.response.data.message);
+
+    }else{
+      alert(err.response.data.message.message);
+    }
+    console.log(err.response.data.message)
+  }}
+  const userPasswordForm = document.querySelector('.form-user-password')
+  if (userPasswordForm){
+    userPasswordForm.addEventListener('submit', e => {
+   e.preventDefault();
+  const passwordCurrent= document.getElementById('password-current').value;
+  const password = document.getElementById('password').value;
+
+  const passwordConfirm = document.getElementById('password-confirm').value;
+  // console.log(name,email)
+  updatePassword( passwordCurrent,passwordConfirm,password );
+        });
+  }
+  
