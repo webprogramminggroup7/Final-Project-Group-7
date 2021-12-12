df6ca49a553ef4977290cd2936f343ffbd9685b6
bhargav-baleja <bhargav.baleja.11@gmail.com>
2021-12-12 02:25:55 -0500

    error handling in date picker

--- public/js/login.js
+++ public/js/login.js
@@ -223,9 +223,24 @@ if (SignUpForm)
     signup(form);
   });
 
-  if(createForm)
+if(createForm)
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+  var yyyy = today.getFullYear();
+  if(dd<10){
+    dd='0'+dd
+  } 
+  if(mm<10){
+    mm='0'+mm
+  } 
+
+  today = yyyy+'-'+mm+'-'+dd;
+  document.getElementById("startDates").setAttribute("min", today);
+
   createForm.addEventListener('submit', e => {
     e.preventDefault();
+
     const startDates = document.getElementById('startDates').value
     const name = document.getElementById('name').value
     const duration = document.getElementById('duration').value
@@ -288,7 +303,22 @@ if (SignUpForm)
     }
   });
 
-  if(updateForm)
+if(updateForm)
+  
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+  var yyyy = today.getFullYear();
+  if(dd<10){
+    dd='0'+dd
+  } 
+  if(mm<10){
+    mm='0'+mm
+  } 
+
+  today = yyyy+'-'+mm+'-'+dd;
+  document.getElementById("startDates").setAttribute("min", today);
+
   updateForm.addEventListener('submit', e => {
     e.preventDefault();
     const id = document.getElementById('tourId').value
