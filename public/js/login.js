70b94bd3bf19e8834e670e16d428ba8248fe0b4b
shivaniraykar <shivaniraykar1997@gmail.com>
2021-12-12 02:43:36 -0500

    tour details in edit review page

--- public/js/login.js
+++ public/js/login.js
@@ -360,21 +360,26 @@ if(reviewForm)
     try {
       const review = document.getElementById('review').value
       const rating = document.getElementById('rating').value
-    let reviewCheck = review.match(/^[0-9]+$/) != null
-    if(reviewCheck == true){
-      throw 'Review cannot contain only numbers'
-    }
-    if(rating<=0 || rating>5){
-      throw 'Rating must be between 1 to 5 inclusive'
-    }
-    const form = new FormData();
-    form.append('review', document.getElementById('review').value)
-    form.append('rating', document.getElementById('rating').value)
-    const files = document.getElementById('reviewImage').files
-    for (var i=0; i < files.length; i++) {
-      form.append('reviewImage', files[i]);
-    }
-    createReview(form)
+      const files = document.getElementById('reviewImage').files
+
+      let reviewCheck = review.match(/^[0-9]+$/) != null
+      if(reviewCheck == true){
+        throw 'Review cannot contain only numbers'
+      }
+      if(rating<=0 || rating>5){
+        throw 'Rating must be between 1 to 5 inclusive'
+      }
+      if(files.length > 2) throw "Your can provide upto 2 images";
+      const form = new FormData();
+      form.append('review', document.getElementById('review').value)
+      form.append('rating', document.getElementById('rating').value)
+      if(files && files.length > 0){
+        for (var i=0; i < files.length; i++) {
+          form.append('reviewImage', files[i]);
+        }
+      }
+      
+      createReview(form)
     }
     catch(e) {
       alert(e)
